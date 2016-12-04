3b5f6e9257beb1a7d4486ea1223df1da520bcde2
mathter <mathter@mail.ru>
2016-12-04 11:23:20 +0700

    Tmp

--- rod-vfs/rod-vfs-api/src/main/java/biz/ostw/ee/vfs/VfsPathId.java
+++ rod-vfs/rod-vfs-api/src/main/java/biz/ostw/ee/vfs/VfsPathId.java
@@ -5,18 +5,66 @@ import java.io.Serializable;
 /**
  * @author mathter
  */
-public class VfsPathId implements Serializable {
-	private static final long serialVersionUID = -4058902084265921709L;
+public class VfsPathId implements Serializable
+{
+    private static final long serialVersionUID = -4058902084265921709L;
 
-	private long id;
+    private Long id;
 
-	private String name;
+    private String name;
 
-	public VfsPathId() {
-	}
+    public VfsPathId()
+    {
+    }
 
-	public VfsPathId(long id, String name) {
-		this.id = id;
-		this.name = name;
-	}
+    public VfsPathId( Long id, String name )
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ( ( id == null ) ? 0 : id.hashCode() );
+        result = prime * result + ( ( name == null ) ? 0 : name.hashCode() );
+        return result;
+    }
+
+    @Override
+    public boolean equals( Object obj )
+    {
+        if ( this == obj )
+            return true;
+        if ( obj == null )
+            return false;
+        if ( getClass() != obj.getClass() )
+            return false;
+        VfsPathId other = (VfsPathId) obj;
+        if ( id == null )
+        {
+            if ( other.id != null )
+                return false;
+        } else if ( !id.equals( other.id ) )
+            return false;
+        if ( name == null )
+        {
+            if ( other.name != null )
+                return false;
+        } else if ( !name.equals( other.name ) )
+            return false;
+        return true;
+    }
 }
